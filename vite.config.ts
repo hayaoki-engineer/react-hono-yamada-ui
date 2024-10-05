6e72af8e6ea493cbdcfeba946050ea7c7683660f
Taisei <taisei.960420@gmail.com>
2024-10-05 19:58:25 +0900

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import devServer, { defaultOptions } from "@hono/vite-dev-server";
+import { defineConfig } from "vite";
+
+export default defineConfig(({ mode }) => {
+  if (mode === "client") {
+    return {
+      build: {
+        rollupOptions: {
+          input: "./src/client.tsx",
+          output: {
+            entryFileNames: "static/client.js",
+          },
+        },
+      },
+    };
+  } else {
+    return {
+      ssr: {
+        external: ["react", "react-dom"],
+      },
+      plugins: [
+        devServer({
+          exclude: ["/*", ...defaultOptions.exclude],
+          entry: "src/index.tsx",
+        }),
+      ],
+    };
+  }
+});
